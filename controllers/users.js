94ddcbe208249300a12ca0a43406f5456a083059
Nikita <filatovnikita1994@yandex.ru>
2022-02-28 18:18:04 +0300

    Доработать по замечаниям ревьюера

--- controllers/users.js
+++ controllers/users.js
@@ -97,7 +97,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000,
         httpOnly: true,
         sameSite: 'none',
-        // secure: true,
+        secure: true,
       }).send({
         name: user.name,
         email: user.email,
